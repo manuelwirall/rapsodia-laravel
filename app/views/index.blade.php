2a357b83118fbe72f88e44b1c263bdd2bc9a26ef
manuelwirall <manuel@wirallinteractive.com.ar>
2014-09-23 12:13:44 -0300

    corrijo y reorganizo js

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -31,14 +31,10 @@
 	<link rel="stylesheet" href="{{ URL::asset('css/app.css') }}">
 	<link rel="stylesheet" href="{{ URL::asset('css/animations.css') }}">
 	<!-- JS -->
-	<script type="text/javascript" src="//assets.pinterest.com/js/pinit.js"></script>
-	<script src='//maps.googleapis.com/maps/api/js?sensor=false'></script>
 	<script src="{{ URL::to('components/jquery.min.js') }}"></script>
 	<script src="{{ URL::to('components/jquery.easing.js') }}"></script>
 	<script src="{{ URL::to('components/owl.carousel.min.js') }}"></script>
 	<script src="{{ URL::to('components/detect-element-resize.js') }}"></script>
-	<script src="{{ URL::to('components/lodash.js') }}"></script>
-	<script src="{{ URL::to('components/lazy.js') }}"></script>
 
 	<script src="{{ URL::to('components/angular.js') }}"></script>
 	<script src="{{ URL::to('components/angular-file-upload-shim.min.js') }}"></script>
@@ -49,11 +45,16 @@
 	<script src="{{ URL::to('components/angular-translate.min.js') }}"></script>
 	<script src="{{ URL::to('components/angular-translate-loader-static-files.min.js') }}"></script>
 	<script src="{{ URL::to('components/angular-gridster.js') }}"></script>
-	<script src="{{ URL::to('components/angular-google-maps.js') }}"></script>
 
 	<script src="{{ URL::to('components/ui-bootstrap.min.js') }}"></script>
 	<script src="{{ URL::to('components/ui-bootstrap-tpls.min.js') }}"></script>
 	
+	<script type="text/javascript" src="//assets.pinterest.com/js/pinit.js"></script>
+	<script src='//maps.googleapis.com/maps/api/js?sensor=false'></script>
+	<script src="{{ URL::to('components/lodash.js') }}"></script>
+	<script src="{{ URL::to('components/angular-google-maps.js') }}"></script>
+	<script src="{{ URL::to('components/lazy.js') }}"></script>
+
 	<script src="{{ URL::to('js/controllers.js') }}"></script>
 	<script src="{{ URL::to('js/app.js') }}"></script>
 	<script src="{{ URL::to('js/filters.js') }}"></script>
