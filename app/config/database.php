f7cb5b97a24b28ce1802e9432870f0b78fe95fa8
manuelwirall <manuel@wirallinteractive.com.ar>
2014-09-23 11:11:40 -0300

    configuro base de datos

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'forge',
-			'username'  => 'forge',
-			'password'  => '',
+			'database'  => 'rapsodia',
+			'username'  => 'rapsodia',
+			'password'  => 'rapsodia',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
