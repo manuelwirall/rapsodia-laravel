e947d7b0891f254f717fa1833d1eabe956dfb139
manuelwirall <manuel@wirallinteractive.com.ar>
2014-09-23 11:31:29 -0300

    agrego componentes y css

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -1,13 +1,12 @@
 <!doctype html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
 	<title>Rapsodia Laravel</title>
-
+	<!-- Meta Configs -->
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<!-- Base uri por pais -->
+	<!-- Country Base HREF -->
 	<base href="/ar/">
 	<!-- AngularJS HTML5Mode -->
 	<meta name="fragment" content="!">
@@ -22,8 +21,17 @@
 	<meta property="og:site_name" content=""/>
 	<meta property="og:description" content=""/>
 	<meta property="og:image" content=""/>
+	<!-- CSS -->
+	<link rel="stylesheet" href="{{ URL::asset('components/bootstrap.css') }}">
+	<link rel="stylesheet" href="{{ URL::asset('components/angular-gridster.min.css.css') }}">
+	<link rel="stylesheet" href="{{ URL::asset('components/angular-social.css') }}">
+	<link rel="stylesheet" href="{{ URL::asset('components/owl.carousel.css') }}">
+	<link rel="stylesheet" href="{{ URL::asset('components/owl.theme.css') }}">
+	<link rel="stylesheet" href="{{ URL::asset('components/sticky-footer.css') }}">
+	<link rel="stylesheet" href="{{ URL::asset('css/app.css') }}">
+	<link rel="stylesheet" href="{{ URL::asset('css/animations.css') }}">
 </head>
 <body>
-	
+
 </body>
 </html>
